5f5f0fcb266e9b1b167c3b06c081577ab373a735
Susan Prestage <susan.prestage@gmail.com>
2025-12-02 14:29:04 -0500

    Working out CI issues on Github

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -3,6 +3,7 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   setup do
     @product = products(:one)
+    @title   = "The Great Book #{rand(1000)}"
   end
 
   test "visiting the index" do
